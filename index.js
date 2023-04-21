42a3eddf17d20e2593c2793bd232bb4742827f71
Jose Luis <joseluis456@gmail.com>
2023-04-21 13:38:02 -0400

    Adding date

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const fs = require ('fs');
 
 const credential = { appState : JSON.parse(fs.readFileSync('appstate.json', 'utf-8')) }
 
+// login({email: "miguel.angel.miranda.espejo@gmail.com", password: "jlhnjlhn"}, (err, api) => {
+//     if(err) return console.error(err);
+
+//     fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+// });
+
 login(credential, (err, api) => {
     if(err) return console.error(err);
 
@@ -11,12 +17,16 @@ login(credential, (err, api) => {
     });
 
     api.listenMqtt((err, message) => {
+        const date = new Date();
+        const fecha = `${ date.getFullYear() }/${ (date.getMonth()+1).toString().padStart(2, '0') }/${ date.getDate().toString().padStart(2, '0') }`
+        const hora = `${ date.getHours().toString().padStart(2, '0') }:${ date.getMinutes().toString().padStart(2, '0') }:${ date.getSeconds().toString().padStart(2, '0') }`
+        console.log(`${ fecha } ${ hora }`);
+
         console.log(message);
 
         if (message && message.body) {
             
         }
-
 
         if (message.type === 'message' && typeof message.body === "string") {
             api.sendMessage("Gracias por escribirme, tu mensaje es:\nalgo más? :like: 👷🏽‍♂️", message.threadID);
